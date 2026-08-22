d0a29a186707a7dd7db67bf6d3fd509bc39c057a
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-08-22 17:53:46 -0300

    Create Main class for time display
    Add Main class to display time objects.

---
+++ src/exercicios/udemy/condicionais/ex04/Main.java
@@ -0,0 +1,13 @@
+package exercicios.udemy.condicionais.ex04;
+
+public class Main {
+    public static void main(String[] args) {
+        Tempo t1 = new Tempo(1, 16, 5);
+        Tempo t2 = new Tempo(2, 8, 19);
+        
+        System.out.println("======== HORÁRIO DE EXPEDIENTE ========");
+    
+        System.out.println(t1.toString());
+        System.out.println(t2.toString());
+    }
+}
